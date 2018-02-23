c87703c92758121546181caf9628a06056e3a34e
viljavi <ville.viljamaa@hiq.fi>
2018-02-23 09:47:05 +0200

    Initial commit

---
+++ Frends.Community.Unzip/Definitions.cs
@@ -0,0 +1,78 @@
+﻿using Frends.Tasks.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Frends.Community.Unzip
+{
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public enum FileExistAction { Error, Overwrite, Rename };
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+    /// <summary>
+    /// Source properties
+    /// </summary>
+    [DisplayName("Source")]
+    public class SourceProperties
+    {
+        /// <summary>
+        /// Full path to the source file
+        /// </summary>
+        [DefaultValue(@"C:\example\file.zip")]
+        [DefaultDisplayType(DisplayType.Text)]
+        public string SourceFile { get; set; }
+        /// <summary>
+        /// Password for the zip file
+        /// </summary>
+        [DefaultValue("")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+    }
+    /// <summary>
+    /// Destination properties
+    /// </summary>
+    [DisplayName("Destination")]
+    public class DestinationProperties
+    {
+        /// <summary>
+        /// Destination directory
+        /// </summary>
+        [DefaultValue(@"C:\example")]
+        [DefaultDisplayType(DisplayType.Text)]
+        public string DirectoryPath { get; set; }
+    }
+    /// <summary>
+    /// Options
+    /// </summary>
+    [DisplayName("Options")]
+    public class Options
+    {
+        /// <summary>
+        /// Action to be taken when destination file/files exist
+        /// </summary>
+        [DefaultValue(FileExistAction.Error)]
+        public FileExistAction DestinationFileExistsAction { get; set; }
+        /// <summary>
+        /// Create destination directory if it does not exist
+        /// </summary>
+        [DefaultValue(false)]
+        public bool CreateDestinationDirectory { get; set; }
+    }
+    /// <summary>
+    /// Output
+    /// </summary>
+    public class Output
+    {
+        /// <summary>
+        /// a List-object of extracted files
+        /// </summary>
+        public List<string> ExtractedFiles { get; set; }
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public Output()
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+        {
+            ExtractedFiles = new List<string>();
+        }
+    }
+}
---
+++ Frends.Community.UnzipTests/UnZipTests.cs
@@ -0,0 +1,167 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Ionic.Zip;
+
+namespace Frends.Community.Unzip.Tests
+{
+    [TestFixture]
+    public class UnZipTests
+    {
+        string[] fileNames = {@"logo1.png", @"logo2.png", @"folder\logo1.png", @"folder\logo2.png", @"folder\folder\folder\logo1.png",
+                                @"folder\folder\folder\logo2.png", @"folder\folder\folder\folder\logo1.png" };
+        //used for testing the rename-option
+        string[] renamedFilenames = {@"logo1(0).png", @"logo2(0).png", @"folder\logo1(0).png", @"folder\logo2(0).png", @"folder\folder\folder\logo1(0).png",
+                                @"folder\folder\folder\logo2(0).png", @"folder\folder\folder\folder\logo1(0).png" };
+
+        //paths to TestIn and TestOut
+        string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestIn\");
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\");
+        List<string> outputFiles;
+        SourceProperties sp;
+        DestinationProperties dp;
+        Options opt;
+
+        [SetUp]
+        public void Setup()
+        {
+            sp = new SourceProperties();
+            dp = new DestinationProperties();
+            opt = new Options();
+            Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Directory.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut"), true);
+        }
+
+        [Test]
+        public void SourceFileDoesNotExist()
+        {
+            //throws System.IO.FileNotFoundException 
+            sp.SourceFile = Path.Combine(inputPath, @"doesnotexist.zip");
+            opt.DestinationFileExistsAction = FileExistAction.Overwrite;
+            dp.DirectoryPath = outputPath;
+            Assert.That(() => UnzipTask.ExtractArchive(sp, dp, opt, new System.Threading.CancellationToken()), Throws.TypeOf<FileNotFoundException>());
+        }
+
+        [Test]
+        public void DestinatioDirectoryNotFound()
+        {
+            //throws directory not found exception
+            //destination directory does not exist and create destination directory == false
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogos.zip");
+            opt.DestinationFileExistsAction = FileExistAction.Error;
+            opt.CreateDestinationDirectory = false;
+            dp.DirectoryPath = Path.Combine(outputPath, @"\doesnot\exist");
+            Assert.That(() => UnzipTask.ExtractArchive(sp, dp, opt, new System.Threading.CancellationToken()), Throws.TypeOf<DirectoryNotFoundException>());
+        }
+
+        [Test]
+        public void CreateDirectory()
+        {
+            //create destination directory if it does not exist
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogos.zip");
+            opt.DestinationFileExistsAction = FileExistAction.Error;
+            opt.CreateDestinationDirectory = true;
+            dp.DirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory\");
+
+            outputFiles = new List<string>();
+            fileNames.ToList().ForEach(x => outputFiles.Add(Path.Combine(dp.DirectoryPath, x)));
+            Output output = UnzipTask.ExtractArchive(sp, dp, opt, new CancellationToken());
+
+            foreach (string s in outputFiles)
+            {
+                Assert.True(File.Exists(s));
+            }
+            Assert.AreEqual(output.ExtractedFiles.Count, 7);
+        }
+
+        [Test]
+        public void ExtractWithPassword()
+        {
+            //extract password protected archive
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogosWithPassword.zip");
+            sp.Password = "secret";
+
+            opt.DestinationFileExistsAction = FileExistAction.Overwrite;
+            opt.CreateDestinationDirectory = true;
+
+            dp.DirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory");
+
+            Output output = UnzipTask.ExtractArchive(sp, dp, opt, new System.Threading.CancellationToken());
+            Assert.True(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory\logo1.png")));
+            Assert.True(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory\logo2.png")));
+        }
+
+        [Test]
+        public void PasswordError()
+        {
+            //Should throw Ionic.Zip.BadPasswordException
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogosWithPassword.zip");
+            opt.DestinationFileExistsAction = FileExistAction.Overwrite;
+            opt.CreateDestinationDirectory = true;
+            dp.DirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory");
+
+            Assert.That(() => UnzipTask.ExtractArchive(sp, dp, opt, new System.Threading.CancellationToken()), Throws.TypeOf<BadPasswordException>());
+        }
+
+        [Test]
+        public void ThrowErrorOnOverwrite()
+        {
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogos.zip");
+
+            Options opt2 = new Options()
+            {
+                DestinationFileExistsAction = FileExistAction.Overwrite,
+                CreateDestinationDirectory = true
+            };
+
+            opt.DestinationFileExistsAction = FileExistAction.Error;
+            opt.CreateDestinationDirectory = true;
+
+            dp.DirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestData\TestOut\new_directory");
+
+            //unzip files to TestOut, so that there are existing files
+            UnzipTask.ExtractArchive(sp, dp, opt2, new CancellationToken());
+
+            Assert.That(() => UnzipTask.ExtractArchive(sp, dp, opt, new System.Threading.CancellationToken()), Throws.TypeOf<Ionic.Zip.ZipException>());
+        }
+
+        [Test]
+        public void OverwriteFiles()
+        {
+            //
+        }
+
+        [Test]
+        public void RenameFiles()
+        {
+            //test for rename-option
+            sp.SourceFile = Path.Combine(inputPath, @"HiQLogos.zip");
+
+            opt.DestinationFileExistsAction = FileExistAction.Rename;
+            opt.CreateDestinationDirectory = true;
+
+            dp.DirectoryPath = outputPath;
+
+            //extract files to TestOut, so that there are existing files
+            UnzipTask.ExtractArchive(sp, dp, opt, new CancellationToken());
+            Output output = UnzipTask.ExtractArchive(sp, dp, opt, new CancellationToken());
+            //create filenames to test against
+            outputFiles = new List<string>();
+            renamedFilenames.ToList().ForEach(x => outputFiles.Add(Path.Combine(dp.DirectoryPath, x)));
+
+            foreach (string s in outputFiles)
+            {
+                Assert.True(File.Exists(s));
+            }
+            Assert.AreEqual(output.ExtractedFiles.Count, 7);
+        }
+    }
+}
